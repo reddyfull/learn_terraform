b79fe4b7742abbcdc2a3b10aff8ec7bf27dfe8ea
reddyfull <reddyfull@gmail.com>
2023-06-22 21:20:53 -0400

    Adding main.tf for resource group and storage account creation

--- main.tf
+++ main.tf
@@ -2,7 +2,15 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
+resource "azurerm_resource_group" "rg" {
+  name     = "DeveloperResourcesri2023Group"
+  location = "East US"
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "developertfstoragesro2023"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
 }
