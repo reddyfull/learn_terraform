0e33ec4939214e8f3c7ee6cb2fe4e8bcc3759377
reddyfull <reddyfull@gmail.com>
2023-06-23 09:07:17 -0400

    Initial commit

--- main.tf
+++ main.tf
@@ -1,16 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 2.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "DeveloperResourcesri2023Group"
-  location = "East US"
-}
-
-resource "azurerm_storage_account" "storage" {
-  name                     = "developertfstoragesro2023"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
+resource "azurerm_resource_group" "example" {
+  name     = "my-resource-group"
+  location = "West US"
 }
