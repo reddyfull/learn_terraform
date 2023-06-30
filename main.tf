bfe315ed878b53701609ceaf5ebc7ce02258be3d
reddyfull <reddyfull@gmail.com>
2023-06-30 11:39:54 -0400

    Your message here

--- main.tf
+++ main.tf
@@ -1,17 +1,24 @@
+# Variables 
+variable "subscription_id" {}
+variable "tenant_id" {}
+variable "client_id" {}
+variable "client_secret" {}
+
 terraform {
+  required_version = ">= 0.14.9"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 2.0"
+      version = "3.63.0"
     }
   }
 }
 
 provider "azurerm" {
   features {}
-}
-
-resource "azurerm_resource_group" "example" {
-  name     = "my-resource-group"
-  location = "West US"
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  features{}
 }
