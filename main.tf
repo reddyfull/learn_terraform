aa1b24299600937cb02a04f80e8caba2ae415551
reddyfull <reddyfull@gmail.com>
2023-07-01 11:32:40 -0400

    Terraform new

--- main.tf
+++ main.tf
@@ -22,31 +22,8 @@ provider "azurerm" {
   features {}
 }
 
-module "resource_group" {
-  source   = "./resource_group"
-  name     = "sriterraformdevenv"
+resource "azurerm_resource_group" "appgrp" {
+  name     = "app-grp-sri2009"
   location = "East US"
 }
 
-module "storage_account" {
-  source              = "./storage_account"
-  name                = "sritfrmstg2019"
-  name2               = "sritfrkali2019"
-  resource_group_name = module.resource_group.name
-  location            = "East US"
-}
-
-resource "azurerm_storage_container" "data" {
-  name = "data"
-  storage_account_name = module.storage_account.name1
-  container_access_type = "blob"
-  
-}
-
-resource "azurerm_storage_blob" "jenkinsfile" {
-  name                   = "Jenkinsfile"
-  storage_account_name   = module.storage_account.name1
-  storage_container_name = azurerm_storage_container.data.name
-  type                   = "Block"
-  source                 = "/Users/sritadip/Documents/learn_terraform/Jenkinsfile"
-}
